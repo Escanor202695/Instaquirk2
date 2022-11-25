38116bcf0839fa30035ad8d48c06d23893dbb5e2
riazakhanda017ksn <riazkahanda@gmail.com>
2022-11-26 01:34:26 +0600

    about section is done and also problem solve in mobile menu

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,13 +6,19 @@ import HomeTwoHeader from "../HomeTwo/HomeTwoHeader/HomeTwoHeader";
 import HomeTwoAchievement from "../HomeTwo/HomeTwoAchievement/HomeTwoAchievement";
 import HomeTwoFooter from "../HomeTwo/HomeTwoFooter/HomeTwoFooter";
 import HomeTwoFaq from "../HomeTwo/HomeTwoFaq/HomeTwoFaq";
+import CommonCtaArea from "../../components/CommonCtaArea/CommonCtaArea";
+import AboutMore from "../../components/AboutMore/AboutMore";
 
 const About = () => {
   const Faq = [
     {
-      question: "Can you help my business grow?",
+      question: "Why should i choose your agency?",
       solution:
-        "He nicked it show off show pick your nose and blow  off such a fiber bleeding well get stuffed mate no biggie, in my flat bugger wind",
+        "There is no work that we neglect at our company.The only thing we can do to help others is to provide them with services whenever possible.",
+    },
+    {
+      question: "Can you help my business grow?",
+      solution: "Our team always ready to help your business.",
     },
     {
       question:
@@ -21,14 +27,18 @@ const About = () => {
         "Our agency team always ready to help  your business and also fixing the problem. You can get our service easily when you have to need.",
     },
   ];
+  //  const aboutSubTitle = `As an unemployment service provider, we assist clients in finding employment. Develop a business or assist in its development. Additionally,
+  //   we offer courses in Web Development, Graphics Design, Full Stack SEO, and Social Media Management & Marketing `
   return (
     <>
       <HomeTwoHeader />
-      <CommonPageHeader title="About Us" subtitle="About" />
+      <CommonPageHeader title="About Us" subtitle="" />
       <HomeAbout />
+      <AboutMore />
       <HomeTwoFaq arrayFaq={Faq} />
       <HomeTwoAchievement />
-      <HomeBrands />
+      {/* <HomeBrands /> */}
+      <CommonCtaArea />
       <HomeTwoFooter />
     </>
   );
