02fdfd21708aeb8ff154a392b131a7fea139b672
riazakhanda017ksn <riazkahanda@gmail.com>
2022-11-28 23:40:39 +0600

    email js problem has been solved out

--- src/pages/Contact/ContactArea/ContactArea.js
+++ src/pages/Contact/ContactArea/ContactArea.js
@@ -1,65 +1,105 @@
-import React from 'react';
-import emailjs from '@emailjs/browser';
-const cors = require('cors')
-import { useRef } from 'react';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+const cors = require("cors");
 
 const ContactArea = () => {
-   const form = useRef();
+  const form = useRef();
 
-      const sendEmail = (e) => {
-         e.preventDefault();
-         console.log("hello")
-         emailjs.sendForm('service_c12ofag', 'template_j6bk8m8', form.current, '-JDOpPEKcqNDhzIhJ')
-         .then((result) => {
-               console.log(result.text);
-               alert("Successful")
-         }, (error) => {
-               console.log(error.text);
-         });
-      };   
-   return (
-      <>
-         <section className="contact__area">
-            <div className="container-fluid p-0">
-               <div className="row g-0">
-                  <div className="col-xl-12">
-                     <div className="contact__map">
-                        <iframe title='contact' src="https://maps.google.com/maps?hl=en&amp;q=Dhaka+()&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed"></iframe>
-                        <div className="contact__wrapper d-md-flex justify-content-between wow fadeInUp" data-wow-delay=".3s">
-                           <div className="contact__info mr-100">
-                              <h3>Get in touch</h3>
-                              <ul>
-                                 <li>
-                                    <h4>Address</h4>
-                                    <p>24 Parking Plaza, Suite 506 <br /> Ardmore, SA 19004</p>
-                                 </li>
-                                 <li>
-                                    <h4>call us</h4>
-                                    <p><a href="tel:(404)-888-123-456">(404) 888 123 456</a></p>
-                                    <p><a href="tel:(204)-888-234-674">(204) 888 234 674</a></p>
-                                 </li>
-                                 <li>
-                                    <h4>Email Address</h4>
-                                    <p><a href="mailto:Info@example.com">Info@example.com</a></p>
-                                 </li>
-                              </ul>
-                           </div>
-                           <div className="contact__form">
-                              <form onSubmit={sendEmail} ref={form}>
-                                 <input type="text" name="name" required placeholder="Your Name" />
-                                 <input type="email" name="email" required placeholder="Your Email" />
-                                 <textarea required placeholder="Your Message" name="message" ></textarea>
-                                 <button type="submit" className="z-btn">Send Message</button>
-                              </form>
-                           </div>
-                        </div>
-                     </div>
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_in1cinu",
+        "template_beh3o3b",
+        e.target,
+        "user_GFZkak7lkFm9Ad6G3J8K1"
+      )
+      .then(
+        (result) => {
+          if (result) {
+            alert("thanks for your message");
+          }
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
+  return (
+    <>
+      <section className="contact__area">
+        <div className="container-fluid p-0">
+          <div className="row g-0">
+            <div className="col-xl-12">
+              <div className="contact__map">
+                <iframe
+                  title="contact"
+                  src="https://maps.google.com/maps?hl=en&amp;q=Dhaka+()&amp;ie=UTF8&amp;t=&amp;z=10&amp;iwloc=B&amp;output=embed"
+                ></iframe>
+                <div
+                  className="contact__wrapper d-md-flex justify-content-between wow fadeInUp"
+                  data-wow-delay=".3s"
+                >
+                  <div className="contact__info mr-100">
+                    <h3>Get in touch</h3>
+                    <ul>
+                      <li>
+                        <h4>Address</h4>
+                        <p>
+                          24 Parking Plaza, Suite 506 <br /> Ardmore, SA 19004
+                        </p>
+                      </li>
+                      <li>
+                        <h4>call us</h4>
+                        <p>
+                          <a href="tel:(404)-888-123-456">(404) 888 123 456</a>
+                        </p>
+                        <p>
+                          <a href="tel:(204)-888-234-674">(204) 888 234 674</a>
+                        </p>
+                      </li>
+                      <li>
+                        <h4>Email Address</h4>
+                        <p>
+                          <a href="mailto:Info@example.com">Info@example.com</a>
+                        </p>
+                      </li>
+                    </ul>
                   </div>
-               </div>
+                  <div className="contact__form">
+                    <form onSubmit={sendEmail} ref={form}>
+                      <input
+                        type="text"
+                        name="name"
+                        required
+                        placeholder="Your Name"
+                      />
+                      <input
+                        type="email"
+                        name="email"
+                        required
+                        placeholder="Your Email"
+                      />
+                      <textarea
+                        required
+                        placeholder="Your Message"
+                        name="message"
+                      ></textarea>
+                      <button type="submit" className="z-btn">
+                        Send Message
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
             </div>
-         </section>
-      </>
-   );
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ContactArea;
